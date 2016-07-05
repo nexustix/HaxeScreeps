2e0c6699edee5dcdda4626d16ec4a340fbcda9ac
Nexustix <nexus1768@tuta.io>
2016-07-05 22:31:10 +0200

    Add basic implementation of OwnedStructure

--- OwnedStructure.hx
+++ OwnedStructure.hx
@@ -1,4 +1,7 @@
 package;
 
-extern class OwnedStructure {
+extern class OwnedStructure extends Structure{
+    public static var my:       Bool;
+    //TODO create Owner class ?
+    public static var owner:    Dynamic;
 }
