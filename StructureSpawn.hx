33c0ff7413c2e2b4f7aa3c281e79ec3abab0b322
Nexustix <nexus1768@tuta.io>
2016-07-05 22:31:41 +0200

    Add basic implementation of StructureSpawn

--- StructureSpawn.hx
+++ StructureSpawn.hx
@@ -1,4 +1,17 @@
 package;
 
-extern class StructureSpawn {
+extern class StructureSpawn extends OwnedStructure {
+    public var energy:          Int;
+    public var energyCapacity:  Int;
+    public var memory:          Dynamic;
+    public var name:            String;
+    public var spawning:        SpawningStatus;
+
+    public function createCreep (body: Array<String>, ?name: String, memory: Dynamic): Int;
+}
+
+class SpawningStatus{
+    public var name:            String;
+    public var needTime:        Int;
+    public var remainingTime:   Int;
 }
