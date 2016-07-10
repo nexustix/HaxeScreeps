0b3f6979fa2da7cdd5743901ea8dacfd265dd189
Nexustix <nexus1768@tuta.io>
2016-07-10 23:50:38 +0200

    Change make "ConstructionSite" use "Owner"

--- ConstructionSite.hx
+++ ConstructionSite.hx
@@ -3,7 +3,7 @@ package;
 extern class ConstructionSite extends RoomObject {
     public var id:              Int;
     public var my:              Bool;
-    public var owner:           Dynamic;
+    public var owner:           Owner;
     public var progress:        Int;
     public var progressTotal:   Int;
     public var structureType:   String;
